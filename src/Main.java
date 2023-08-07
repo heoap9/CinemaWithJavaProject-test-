205e2a07ed4844f810d905f41949966279c59e30
J <heoap9@gmail.com>
2023-08-07 14:49:52 +0900

    Merge remote-tracking branch 'origin/master'

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@
 import  Movie.DefaultMovie;
 import  User.Guest;
 
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +13,23 @@ public class Main {
 
         Guest u1 = new Guest("성근","1234");
 
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
+
+        switch (input.charAt(0)){
+            case'1':
+
+                break;
+            case'2':
+                break;
+            case'3':
+                break;
+            case'4':
+                break;
+            default:
+                System.out.println("잘못입력하셨습니다");
+        }
         System.out.println(u1.id);
 
 
