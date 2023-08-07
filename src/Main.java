3a6ea6db5a80341824bf87da677a3bb4f7133b91
J <heoap9@gmail.com>
2023-08-07 14:36:33 +0900

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import  Movie.DefaultMovie;
+import  User.Guest;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("췌아!");
+
+        Guest u1 = new Guest("성근","1234");
+
+
+        System.out.println(u1.id);
+
+
+
+
     }
 }
