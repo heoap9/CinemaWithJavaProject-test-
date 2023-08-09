5c23d3e8e3603bd343b29dca742a91e6f19641cd
heoap <heoap9@gmail.com>
2023-08-09 15:30:50 +0900

    Merge remote-tracking branch 'origin/master'

--- src/Movie/Movie.java
+++ src/Movie/Movie.java
@@ -1,5 +1,7 @@
 package Movie;
 
+import User.Member;
+
 /**
  * 영화 정보를 저장하기 위한 클래스!
  *  저장되는 정보는 영화이름,시간,가격,좌석,좌석에 배정된 초기값,남은 입석수 등이 제공된다
@@ -31,6 +33,22 @@ public class Movie {
         this.movieseatpreset = 60; // 변수 이름 수정
         this.movieseatprec = movieseatpreset;
         this.movieseat = new boolean[movieseatpreset /10][10]; // 배열 생성 및 초기화 추가
+    }
+
+    @Override
+    public int hashCode(){ //id 값을 비교하여 같은 값이 저장된다면 저장시키지 않는다.
+        return moviename.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) { //id의 문자열 자체를 비교하여 값이 다르다면 저장시키지 않는다.
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Movie target) {
+            return moviename.equals(target.moviename);
+        }
+        return false;
     }
 
 
