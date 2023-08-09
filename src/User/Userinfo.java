0259998910798c9d3fb6b4e0eccd30eb9cdc19be
heoap <heoap9@gmail.com>
2023-08-09 11:18:43 +0900

    ronal 님이 변경한 내용

--- src/User/Userinfo.java
+++ src/User/Userinfo.java
@@ -4,17 +4,27 @@ import User.*;
 
 public class Userinfo {
 
+
+    //member에 저장된 정보를 가져와 유저의 예매 내용을 출력 하는 메소드
     public static void userView(Member member){
 
         String Ticket;
+        String userid;
+
+        userid = member.id;
         Ticket = member.haveticket;
-        String a = Ticket.replace("null", "");
+
+        String a = Ticket.replace("null", "");    //앞에 저장된 null이 출력 되어 삭제하기 위한 코드 입니다.
         String[] arr = Ticket.split(",");
         for(String token : arr){
 
             //System.out.println(token);
         }
+
+        System.out.println("==================================");
+        System.out.println(userid + "님의 예매 내역");
         System.out.println(a);
+        System.out.println("==================================");
 
     }
 
