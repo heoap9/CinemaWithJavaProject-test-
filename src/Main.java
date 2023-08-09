33d439f731f66938604662f6d6d611f48ebd57d3
heoap <heoap9@gmail.com>
2023-08-09 17:09:21 +0900

    Merge remote-tracking branch 'origin/master'

--- src/Main.java
+++ src/Main.java
@@ -30,29 +30,33 @@ public class Main {
         String input = null;
         String input1 = null;
 
-        while (select1) {
-
+        while (select) { //비회원 메뉴
             Menu.mainMenu(Accountsave);
-            input = scanner.nextLine();
-
             while (Accountsave != null) {//회원 로그인 시
                 Menu.mainMenu(Accountsave);
                 input = scanner.nextLine();
                 switch (input.charAt(0)) {
-                    case '1':
+                    case '1': //예매
                         Menu.movieMenu(movie1);
                         System.out.println("좌석을 선택해주세요");
                         input1 = scanner.nextLine();
                         MovieFunction.BuyTiket(Accountsave, movie1, input1);
                         break;
 
-                    case '2':
+                    case '2'://예매 취소
+
                         break;
 
                     case '3':
                         Menu.movieTiketprintwithUser(Accountsave);
                         break;
                     case '4':
+                        System.out.println("적립금 입금 메뉴 입니다");
+                        System.out.println("얼마를 입금하시겠습니까?");
+                        System.out.println("->");
+                        MemberFunction.MemberMoneyInput(Accountsave,input);
+                        break;
+                    case '5':
                         Accountsave= null;
                         input =null;
                         break;
@@ -62,13 +66,12 @@ public class Main {
 
 
 
+            input = scanner.nextLine();
             switch (input.charAt(0)) {  //비회원 로그인 시
-                case '1':   /**로그인 메뉴**/
-                    /**진입시 회원가입,로그인 창으로 진입**/
-                    select = true;
-                    while (select) {
+                case '1' -> {   /**로그인 메뉴**/
+                                /**진입시 회원가입,로그인 창으로 진입**/
+                    while (select1) {
                         Menu.loginMenu();
-                        scanner = new Scanner(System.in);
                         input = scanner.nextLine();
                         switch (input.charAt(0)) {
                             case '1':
@@ -81,7 +84,7 @@ public class Main {
                                 pw = scanner.nextLine();
                                 Accountsave = FindGuest.FindAccount(id, pw);
                                 if (Accountsave != null) {
-                                    select = false;
+                                    select1 = false;
                                 }
                                 break;
                             case '2':
@@ -92,11 +95,14 @@ public class Main {
                                 id = scanner.nextLine();
                                 System.out.print("PASSWORD : ");
                                 pw = scanner.nextLine();
-                                MakeMember.setMember(new Member(id, pw));
+                                if(MakeMember.setMember(new Member(id, pw))) {
+                                    System.out.println("회원가입을 축하드립니다!");
+                                    System.out.println("회원가입 축하금으로 3000포인트를 입금해드립니다");
+                                }
                                 break;
                             case '3':
                                 System.out.println("이전 화면으로 돌아갑니다");
-                                select = false;
+                                select1 = false;
                                 break;
                             default:
                                 System.out.println("잘 못 입력하셨습니다");
@@ -104,36 +110,27 @@ public class Main {
                         }
 
                     }
-                    break;
-
-
-                case '2':
+                }
+                case '2' -> {
                     Menu.showMovienameList();
                     System.out.println("영화선택:-->");
-                    input1 = scanner.nextLine();
-                    SaveMovie = FindMovie.SelectFindMovie(input1);
+                    input = scanner.nextLine();
+                    SaveMovie = FindMovie.SelectFindMovie(input);
                     System.out.println();
                     System.out.println("┌─────────── 상영중 ───────────┐");
                     System.out.println("│                             │");
                     System.out.println(SaveMovie.movietime); //해당되는 영화의
+
                     // 모든 시간을 출력해야함 (메서드 구현)
                     System.out.println("│                             │");
                     System.out.println("└─────────────────────────────┘");
                     System.out.print("시간 선택 ->  ");
-
-
-                    break;
-
-
-                case '3':
+                }
+                case '3' -> {
                     System.out.println("종료합니다");
                     select1 = false;
-                    break;
-
-                default:
-                    System.out.println("잘 못 입력하셨습니다");
-
-
+                }
+                default -> System.out.println("잘 못 입력하셨습니다");
             }
         }
 
