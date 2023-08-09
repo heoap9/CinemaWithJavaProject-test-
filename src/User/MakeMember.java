fa60bfc893a5591fa22621cba46fc86f85677f4c
heoap <heoap9@gmail.com>
2023-08-09 15:30:12 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/User/MakeMember.java
@@ -0,0 +1,25 @@
+package User;
+
+import java.util.HashSet;
+import java.util.*;
+
+public class MakeMember {
+    public static Set<Member> set = new HashSet<Member>();
+
+
+    /**
+     * @param member 컬렉션 구조를 사용하여
+     *               id값이 같다면 저장되지 않게 구현한 계정 생성 메서드 이다
+     * @return      계정이 저장됬다면  return 1, 아니라면  return 0
+     */
+    public static int setMember(Member member){
+        if(set.add (member)){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+
+
+}
