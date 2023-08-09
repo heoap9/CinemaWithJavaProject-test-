4c7ba740ad551c005b4e28347d87a6d181f28609
heoap <heoap9@gmail.com>
2023-08-09 16:40:36 +0900

    Merge remote-tracking branch 'origin/master'

--- src/Movie/Movie.java
+++ src/Movie/Movie.java
@@ -30,6 +30,15 @@ public class Movie {
     public Movie(String moviename, int movieprice) {
         this.moviename = moviename;
         this.movieprice = movieprice;
+        this.movietime = "09:00";
+        this.movieseatpreset = 60; // 변수 이름 수정
+        this.movieseatprec = movieseatpreset;
+        this.movieseat = new boolean[movieseatpreset /10][10]; // 배열 생성 및 초기화 추가
+    }
+    public Movie(String moviename, int movieprice,String movietime) {
+        this.moviename = moviename;
+        this.movieprice = movieprice;
+        this.movietime = movietime;
         this.movieseatpreset = 60; // 변수 이름 수정
         this.movieseatprec = movieseatpreset;
         this.movieseat = new boolean[movieseatpreset /10][10]; // 배열 생성 및 초기화 추가
