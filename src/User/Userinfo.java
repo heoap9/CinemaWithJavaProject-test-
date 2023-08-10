61e537672cc08e9c3c3dcc592615d5635c30d841
heoap <heoap9@gmail.com>
2023-08-10 16:43:46 +0900

    ronal 님이 변경한 내용

--- src/User/Userinfo.java
+++ src/User/Userinfo.java
@@ -1,5 +1,14 @@
 package user;
 
+import movie.Movie;
+import tiket.Tiket;
+import user.*;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 public class Userinfo {
 
 
